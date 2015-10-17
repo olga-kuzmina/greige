2f599b31ad998dc13864bd6f8b0a89f0d78aca24
olga-kuzmina <olga-kuzmina@users.noreply.github.com>
2015-10-17 18:42:00 +0200

    Added files uploading

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/recolor', methods=["POST"])
+def recolor():
+    uploaded_files = request.files.getlist("files")
+    return ""
