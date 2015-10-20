f9dd2808b92556af7bcb8de3ef13ccf9c0fddeb2
olga-kuzmina <olga-kuzmina@users.noreply.github.com>
2015-10-20 10:27:20 +0200

    Mailing greige results

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_mail import Mail
 
-app = Flask(__name__)
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+mail = Mail(app)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
 from flask import render_template, request
 from app import app
+from utils import greige, send_mail
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/recolor', methods=["POST"])
 def recolor():
     uploaded_files = request.files.getlist("files")
-    return ""
+    color = request.form.get('color')
+    email = request.form.get('email')
+
+    greiged_files = []
+    for u_file in uploaded_files:
+        f_name, f_extension = u_file.filename.split('.')
+        new_filename = f_name + '_%s.' % color[1:] + f_extension
+        greiged_files.append({'name': new_filename, 'file': greige(u_file, color)})
+
+    send_mail(email, greiged_files)
+    return ''
