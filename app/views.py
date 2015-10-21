931b1991483110774371bcd26fab705f340a47cf
olga-kuzmina <olga-kuzmina@users.noreply.github.com>
2015-10-21 17:39:00 +0200

    Implemented multiple coloring

--- app/views.py
+++ app/views.py
@@ -10,14 +10,16 @@ def index():
 @app.route('/recolor', methods=["POST"])
 def recolor():
     uploaded_files = request.files.getlist("files")
-    color = request.form.get('color')
+    colors = set(request.form.getlist('color'))
     email = request.form.get('email')
 
     greiged_files = []
     for u_file in uploaded_files:
         f_name, f_extension = u_file.filename.split('.')
-        new_filename = f_name + '_%s.' % color[1:] + f_extension
-        greiged_files.append({'name': new_filename, 'file': greige(u_file, color)})
+        for color in colors:
+            if color:
+                new_filename = f_name + '_%s.' % color[1:] + f_extension
+                greiged_files.append({'name': new_filename, 'file': greige(u_file, color)})
 
     send_mail(email, greiged_files)
     return ''
